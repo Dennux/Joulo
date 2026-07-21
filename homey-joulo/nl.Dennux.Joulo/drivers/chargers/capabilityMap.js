f720a2eb41bee3d2235c2e4c013a5591bacf8895
Dennux <Dennis@dennux.nl>
2026-07-21 08:54:08 +0200

    feat: add charger information capabilities

--- homey-joulo/nl.Dennux.Joulo/drivers/chargers/capabilityMap.js
+++ homey-joulo/nl.Dennux.Joulo/drivers/chargers/capabilityMap.js
@@ -5,5 +5,9 @@ module.exports = {
     onoff: model => model.isCharging,
 
     meter_power: model => model.latestMeterWh / 1000,
+     charger_status: model => model.status,
+     charger_connection_type: model => model.connectionType,
+     charger_mid_certified: model => model.midCertified,
+     charger_mid_eligible: model => model.midEligible,
 
-};cd
+};
